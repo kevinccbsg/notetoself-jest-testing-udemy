0ba8536f79c9dbe5585b219012c3987a640d4fc4
kevinccbsg <kevinccbsg@gmail.com>
2018-05-17 00:43:03 -0400

    Change  - Function created to controll pdf creation depending on headless NODE_ENV

--- ui-test/createNotes.ui.js
+++ ui-test/createNotes.ui.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
+const path = require('path');
 const iPhone = devices['iPhone 6'];
 
 const props = {
@@ -19,20 +20,32 @@ const browserConfig = {
   gotoConfig: { waitUntil: 'load' },
 };
 
+const dirUIPdfs = path.join(__dirname, '..', 'user-stories', 'create-note');
+
 jest.setTimeout(10000);
 
 describe('UI test for create note functionality', () => {
   let browser;
   let page;
+  let pdfProxy;
   beforeAll(async () => {
     browser = await puppeteer.launch(browserConfig.launch);
     page = await browser.newPage();
     await page.emulate(iPhone);
     await page.goto(browserConfig.urlGoTo, browserConfig.gotoConfig);
+    await page.emulateMedia('screen');
+    pdf = async (params) => {
+      if (browserConfig.launch.headless === false) {
+        return Promise.resolve();
+      }
+      const result = await page.pdf(params);
+      return result;
+    };
   });
 
   it('Load the page with title `Note to self`', async () => {
     const text = await page.evaluate(() => document.body.textContent);
+    await pdf({ path: `${dirUIPdfs}/render-view.pdf`, width: iPhone.viewport.width });
     expect(text).toContain(props.titleAPP);
   });
 
@@ -46,9 +59,11 @@ describe('UI test for create note functionality', () => {
     const input = await page.$('form input');
     await input.tap();
     await page.keyboard.type(props.newNote, { delay: 100 });
+    await pdf({ path: `${dirUIPdfs}/field-complete.pdf` });
     await button.tap();
     const text = await page.evaluate(() => document.body.textContent);
     expect(text).toContain(props.newNote);
+    await pdf({ path: `${dirUIPdfs}/new-note.pdf` });
     const cookies = await page.cookies();
     const isCookies = !!cookies.length;
     expect(isCookies).toBe(true);
@@ -61,12 +76,15 @@ describe('UI test for create note functionality', () => {
     const [button, deleteButton] = await page.$$('button');
     await input.tap();
     await page.keyboard.type(props.newNote, { delay: 100 });
+    await pdf({ path: `${dirUIPdfs}/second-field-complete.pdf` });
     await button.tap();
+    await pdf({ path: `${dirUIPdfs}/second-new-note.pdf` });
     await page.waitFor(1000);
     await deleteButton.tap();
     const cookies = await page.cookies();
     const isCookies = !!cookies.length;
     expect(false).toBe(false);
+    await pdf({ path: `${dirUIPdfs}/delete-notes.pdf` });
   });
 
   afterAll(async () => {
